e59696fe2055d1d46edfb6a7267db6b06269149a
Jordi Burgos <jordiburgos@gmail.com>
2020-08-25 21:17:44 +0200

    Configure build with TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './excellentexport.js',
+    entry: './excellentexport.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'excellentexport.js',
@@ -10,7 +10,24 @@ module.exports = {
         libraryExport: 'default',
         auxiliaryComment: 'ExcellentExport.js'
     },
-    performance: {
-        hints: false
+  
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+  
+    resolve: {
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+    },
+  
+    module: {
+      rules: [
+        // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+        { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+  
+        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+        { test: /\.js$/, loader: "source-map-loader" }
+      ]
     }
-};
+  
+    // Other options...
+  };
